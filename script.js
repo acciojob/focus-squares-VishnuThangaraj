434bc2aad7d67c533be5f72cfdc8d95a2976a0fc
Vishnu Thangaraj <110882983+VishnuThangaraj@users.noreply.github.com>
2024-02-28 20:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+function highlightSquares(activeSquare){
+	for(let i=1; i<=3; i++){
+		if(i != activeSquare){
+			document.getElementById(`square${i}`).style.backgroundColor = '#6F4E37';
+		}
+	}
+}
+
+function resetSquares(){
+	for(let i=1; i<=3; i++){
+			document.getElementById(`square${i}`).style.backgroundColor = '#E6E6FA';
+	}
+}
